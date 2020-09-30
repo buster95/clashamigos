3c8e1b52c7259cb5b0087aab29b99a8dd5fb1f00
Walter Corrales <corraleswalter@live.com>
2020-09-29 21:11:09 -0600

    current war design 😎

--- src/Components/Guerras/Guerras.tsx
+++ src/Components/Guerras/Guerras.tsx
@@ -10,21 +10,90 @@ import Chip from '@material-ui/core/Chip';
 import { Box } from '@material-ui/core';
 
 const Guerras: React.FC = () => {
-   const [wars, setWars] = useState([]);
+   const [currentWar, setCurrentWar] = useState<any>({});
+   const [wars, setWars] = useState<any[]>([]);
 
    useEffect(() => {
       cocapi.wars()
          .then(res => setWars(res.items))
          .catch(err => console.log(err));
+
+      cocapi.currentWar()
+         .then(res => setCurrentWar(res))
+         .catch(err => console.log(err))
    }, []);
 
    return (
       <Fragment>
+         <Typography variant="h5" style={{ display: (currentWar.state ? '' : 'none'), marginBottom: 25 }}>
+            Guerra Actual
+         </Typography>
+
+         <Paper style={{ display: (currentWar.state ? '' : 'none'), marginBottom: 25 }}>
+            <Box display="flex" p={1}>
+               <Box width="50%" className="card-war">
+                  <Box display="flex" p={1}>
+                     <Box className="card-flag">
+                        <img src={currentWar.clan?.badgeUrls?.medium} alt="clash of clans league" loading="lazy" />
+                     </Box>
+
+                     <Box width="100%">
+                        <Typography variant="body1">
+                           {currentWar.clan?.name}
+                           <br />
+                           <Typography variant="caption">
+                              <a href={"/clan/" + encodeURIComponent(currentWar.clan?.tag)}>
+                                 {currentWar.clan?.tag}
+                              </a>
+                           </Typography>
+                        </Typography>
+                     </Box>
+                  </Box>
+                  <Chip className="card-war-result" label={'Lvl ' + currentWar.clan?.clanLevel} />
+                  <hr />
+                  <Typography variant="body1">ESTRELLAS: {currentWar.clan?.stars}</Typography>
+                  <hr />
+                  <Typography variant="body1">ATAQUES: {currentWar.clan?.attacks}</Typography>
+                  <hr />
+                  <Typography variant="body1">PORC. DESTRUCCIÓN: {String(currentWar.clan?.destructionPercentage).slice(0, 5)}</Typography>
+               </Box>
+
+               <Box alignSelf="center">
+                  <Typography variant="h4" align="center">VS</Typography>
+                  <Typography variant="h6" align="center">{currentWar.state !== 'warEnded' ? 'EN PROCESO' : 'TERMINADA'}</Typography>
+               </Box>
+
+               <Box width="50%" className="card-war" alignContent="right" alignItems="right" textAlign="right">
+                  <Box display="flex" p={1}>
+                     <Box width="100%">
+                        <Typography variant="body1" align="right">
+                           {currentWar.opponent?.name}
+                           <br />
+                           <Typography variant="caption">
+                              <a href={"/clan/" + encodeURIComponent(currentWar.opponent?.tag)}>
+                                 {currentWar.opponent?.tag}
+                              </a>
+                           </Typography>
+                        </Typography>
+                     </Box>
+
+                     <Box className="card-flag">
+                        <img src={currentWar.opponent?.badgeUrls?.medium} alt="clash of clans league" loading="lazy" />
+                     </Box>
+                  </Box>
+                  <Chip className="card-war-result" label={'Lvl ' + currentWar.opponent?.clanLevel} />
+                  <hr />
+                  <Typography variant="body1" align="right">ESTRELLAS: {currentWar.opponent?.stars}</Typography>
+                  <hr />
+                  <Typography variant="body1" align="right">ATAQUES: {currentWar.opponent?.attacks}</Typography>
+                  <hr />
+                  <Typography variant="body1" align="right">PORC. DESTRUCCIÓN: {String(currentWar.opponent?.destructionPercentage).slice(0, 5)}</Typography>
+               </Box>
+            </Box>
+         </Paper>
+
          <Typography variant="h5">
             Guerras del Clan
-            <a href="/wars/current" className="enlace-current">
-               <Chip label="Current" />
-            </a>
          </Typography>
          <br />
          <div style={{ display: (wars.length > 0 ? 'none' : 'flex'), justifyContent: 'center', marginTop: 20 }}>
@@ -36,7 +105,7 @@ const Guerras: React.FC = () => {
                {
                   wars.map((item: any, index) => (
                      <Grid item key={index} xs={6} sm={4} md={3}>
-                        <Paper className="card-war">
+                        <Paper className="card-war withHover">
                            <Box display="flex" p={1}>
                               <Box className="card-flag">
                                  <img src={item.opponent.badgeUrls.small} alt="clash of clans league" loading="lazy" />
@@ -47,7 +116,7 @@ const Guerras: React.FC = () => {
                                     {item.opponent.name}
                                     <br />
                                     <Typography variant="caption">
-                                       <a target="_blank" rel="noopener noreferrer" href={"/clan/" + item.opponent.tag}>
+                                       <a href={"/clan/" + encodeURIComponent(item.opponent.tag)}>
                                           {item.opponent.tag}
                                        </a>
                                     </Typography>
